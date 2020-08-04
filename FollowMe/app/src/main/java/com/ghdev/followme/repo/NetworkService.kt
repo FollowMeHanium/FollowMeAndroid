ed945b74018a5dfa9d34288b283edd09e7762d9d
mareun <44865268+mareun@users.noreply.github.com>
2020-08-04 15:15:57 +0900

    mypick_dip

--- FollowMe/app/src/main/java/com/ghdev/followme/repo/NetworkService.kt
+++ FollowMe/app/src/main/java/com/ghdev/followme/repo/NetworkService.kt
@@ -1,6 +1,7 @@
 package com.ghdev.followme.repo
 
 import com.ghdev.followme.data.PostLoginResponse
+import com.ghdev.followme.data.PostShopResponse
 import com.ghdev.followme.data.PostSignUpResponse
 import com.google.gson.JsonObject
 import retrofit2.Call
@@ -23,6 +24,20 @@ interface NetworkService {
         @Header("Content-Type") content_type: String,
         @Body() body : JsonObject
     ) :Call<PostLoginResponse>
+
+    //Shop 찜하기
+    @POST("/shop/dip")
+    fun postShopDipResponse(
+        @Header("Content-Type") content_type: String,
+        @Body() body: JsonObject
+    ) : Call<PostShopResponse>
+
+    //Shop 찜 취소
+    @POST("/shop/undip")
+    fun postShopUnDipResponse(
+        @Header("Content-Type") content_type: String,
+        @Body() body: JsonObject
+    ) : Call<PostShopResponse>
 
 
 
--- FollowMe/app/src/main/java/com/ghdev/followme/ui/mypage/MyPickPlaceRecyclerViewAdapter.kt
+++ FollowMe/app/src/main/java/com/ghdev/followme/ui/mypage/MyPickPlaceRecyclerViewAdapter.kt
@@ -4,12 +4,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.contentValuesOf
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ghdev.followme.R
 import com.ghdev.followme.data.test.PlaceInfo
+import com.ghdev.followme.ui.mypage.MypageMypickActivity.Companion.editmode_change
+import okhttp3.internal.notify
+import okhttp3.internal.notifyAll
 
 class MyPickPlaceRecyclerViewAdapter (
     val dataList: ArrayList<PlaceInfo>,
@@ -22,7 +27,7 @@ class MyPickPlaceRecyclerViewAdapter (
     ): MyPickPlaceRecyclerViewAdapter.Holder {
 
         val view: View = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_hot_place, viewGroup, false)
+            .inflate(R.layout.item_mypick, viewGroup, false)
 
         return Holder(view)
     }
@@ -34,23 +39,37 @@ class MyPickPlaceRecyclerViewAdapter (
 
         holder.placename.text = info.name
         holder.address.text = info.address
-
-        /*Glide.with(ctx).load(dataList[position].img)
-            .placeholder(R.drawable.ic_home_black)
-            .into(holder.imgurl) */
+        //holder.star.rating = dataList[position].star.toFloat()
         Glide.with(holder.itemView.context).load(info.img).into(holder.imgurl)
 
+        /*if(editmode_change == false) {
+            holder.container_checked.visibility == View.GONE
+            //notifyDataSetChanged()
+        }*/
+
         holder.container.setOnClickListener {
-            //##detailview로 갈 수 있도록 함
-            dataListClick(info)
+            if(editmode_change == false){
+                //##detailview로 갈 수 있도록 함
+                dataListClick(info)
+            }
+            else if(editmode_change == true){
+                if(holder.container_checked.visibility == View.VISIBLE){
+                    holder.container_checked.visibility = View.GONE
+                }
+                else if(holder.container_checked.visibility == View.GONE){
+                    holder.container_checked.visibility = View.VISIBLE
+                }
+            }
+
         }
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgurl = itemView.findViewById(R.id.img_hot_place_item) as ImageView
-        var placename = itemView.findViewById(R.id.tv_place_name_item) as TextView
-        var address = itemView.findViewById(R.id.tv_place_address_item) as TextView
-        var container = itemView.findViewById(R.id.cl_hot_place_container) as ConstraintLayout
+        var imgurl = itemView.findViewById(R.id.iv_mypick_item_img) as ImageView
+        var placename = itemView.findViewById(R.id.tv_mypick_item_title) as TextView
+        //var star = itemView.findViewById(R.id.rb_star_mycourse_item) as RatingBar
+        var address = itemView.findViewById(R.id.tv_mypick_item_address) as TextView
+        var container = itemView.findViewById(R.id.cl_mypick_container) as ConstraintLayout
 
         //btn
         var btn_unchecked = itemView.findViewById(R.id.btn_mypick_editmode_unchecked) as ImageView

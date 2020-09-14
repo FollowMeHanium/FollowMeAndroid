5432245d1ca7e94c9614abb8f3e83687237d28d9
mareun <44865268+mareun@users.noreply.github.com>
2020-09-14 16:22:14 +0900

    placedetailview_dialog

--- FollowMe/app/src/main/java/com/ghdev/followme/ui/PlaceDetailActivity.kt
+++ FollowMe/app/src/main/java/com/ghdev/followme/ui/PlaceDetailActivity.kt
@@ -1,9 +1,11 @@
 package com.ghdev.followme.ui
 
+import android.animation.Animator
 import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.EditText
 import android.widget.LinearLayout
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.ghdev.followme.R
+import com.ghdev.followme.data.test.MyCourseListInfo
 import com.ghdev.followme.data.test.PlaceInfo
 import com.ghdev.followme.ui.home.HomeFragment.Companion.PLACE_INFO
 import com.ghdev.followme.data.test.ReviewInfo
@@ -37,15 +40,16 @@ class PlaceDetailActivity : AppCompatActivity(), View.OnClickListener {
         Glide.with(this).load(place_info.img).into(iv_place_detail_main)
 
         init()
-        PlaceReviewRcycler()
+        PlaceReviewRecycler()
     }
 
     private fun init(){
         btn_place_detail_add_review.setOnClickListener(this)
         btn_place_detail_add_mypick.setOnClickListener(this)
+        btn_place_detail_add_mycourse.setOnClickListener(this)
     }
 
-    fun PlaceReviewRcycler(){
+    fun PlaceReviewRecycler(){
         reviewList.add(ReviewInfo("aaaaaa", "동해물과 백두산이 마르고 닳도록~", 5))
         reviewList.add(ReviewInfo("aaaaaa", "동해물과 백두산이 마르고 닳도록~", 5))
         reviewList.add(ReviewInfo("aaaaaa", "동해물과 백두산이 마르고 닳도록~", 5))
@@ -59,39 +63,69 @@ class PlaceDetailActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v){
             btn_place_detail_add_review ->{
-                ReviewDialogCreate(v!!)
+                ReviewDialogCreate()
             }
 
             btn_place_detail_add_mypick ->{
                 ClickMypick()
             }
+
+            btn_place_detail_add_mycourse ->{
+                MyCourseDialogCreate()
+            }
         }
     }
 
-    fun ReviewDialogCreate(view: View){
+    fun ReviewDialogCreate(){
         val builder = AlertDialog.Builder(this)
         val dv = layoutInflater.inflate(R.layout.dialog_review_insert, null)
         builder.setView(dv).show()
-
     }
 
     fun ClickMypick(){
 
         val anim = AnimationUtils.loadAnimation(this, R.anim.anim_mypick_alpha)
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                iv_place_detail_anim_mypick.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                iv_place_detail_anim_mypick.visibility = View.VISIBLE
+            }
+
+        })
 
         //mypick가 선택되어져 있을때
         if(mypick){
             btn_place_detail_add_mypick.setImageResource(R.drawable.btn_add_mypick)
-            Toast.makeText(this, "찜 리스트에서 삭제되었습니다.", Toast.LENGTH_LONG)
+            Toast.makeText(this, "찜 리스트에서 삭제되었습니다.", Toast.LENGTH_SHORT).show()
             mypick = false
         }
         //mypick가 선택되어있지 않을때
         else{
             btn_place_detail_add_mypick.setImageResource(R.drawable.btn_selected_mypick)
-            iv_place_detail_anim_mypick.visibility = View.VISIBLE
             iv_place_detail_anim_mypick.startAnimation(anim)
-            iv_place_detail_anim_mypick.visibility = View.GONE
+            Toast.makeText(this, "찜 리스트 추가되었습니다.", Toast.LENGTH_SHORT).show()
             mypick = true
         }
+
+    }
+
+    fun MyCourseDialogCreate() {
+        val builder = AlertDialog.Builder(this)
+        val mycourse = arrayOf("내가 가고싶은 코스1", "내가 가고싶은 코스2")
+        val checkedItem = 1 // cow
+        builder.setSingleChoiceItems(mycourse, checkedItem) { dialog, which ->
+            // user checked an item
+        }
+        builder.setPositiveButton("확인"){dialog, which ->
+            //user clicked OK
+        }
+        builder.setNegativeButton("취소", null)
+        val dialog = builder.create()
+        dialog.show()
     }
 }
